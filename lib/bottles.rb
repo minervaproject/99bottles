9d41d27b862ce83778faaacc4ee10a07d4af823e
Jan Nelson <jan@minervaproject.com>
2017-07-19 10:57:03 -0700

    Extract Song class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,34 +1,19 @@
 require_relative "./vessel"
 require_relative "./inventory"
+require_relative "./song"
 
 class Bottles
 
   def song
-    verses(99, 0)
+    Song.new(99, 0).verses.join("\n")
   end
 
   def verses(starting, ending)
-    starting.downto(ending).map {|i| verse(i)}.join("\n")
+    Song.new(starting, ending).verses.join("\n")
   end
 
   def verse(number)
-    vessel = Vessel.new("bottle", "beer")
-    inventory = Inventory.new(vessel, number)
-
-    case number
-    when 0
-      command = "Go to the store and buy some more"
-      next_inventory = Inventory.new(vessel, 99)
-    else
-      pronoun = number == 1 ? "it" : "one"
-      command = "Take #{pronoun} down and pass it around"
-      next_inventory = Inventory.new(vessel, number - 1)
-    end
-
-    "#{inventory.to_s.capitalize} on the wall, " +
-    "#{inventory}.\n" +
-    "#{command}, " +
-    "#{next_inventory} on the wall.\n"
+    Song.new(number, number).verses.first
   end
 
 end
---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song
+  attr_reader :starting, :ending
+
+  def initialize(starting, ending)
+    @starting = starting
+    @ending = ending
+  end
+
+  def verses
+    starting.downto(ending).map {|i| verse(i)}
+  end
+
+  def verse(number)
+    vessel = Vessel.new("bottle", "beer")
+    inventory = Inventory.new(vessel, number)
+
+    case number
+    when 0
+      command = "Go to the store and buy some more"
+      next_inventory = Inventory.new(vessel, 99)
+    else
+      pronoun = number == 1 ? "it" : "one"
+      command = "Take #{pronoun} down and pass it around"
+      next_inventory = Inventory.new(vessel, number - 1)
+    end
+
+    "#{inventory.to_s.capitalize} on the wall, " +
+      "#{inventory}.\n" +
+      "#{command}, " +
+      "#{next_inventory} on the wall.\n"
+  end
+end
